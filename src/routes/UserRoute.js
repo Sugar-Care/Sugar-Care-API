fddfdb7fa7ba13403aee6f65e661431fb5517b12
Arif Muhammad Laduni <arifmuhammadladuni4@gmail.com>
2024-12-02 20:22:09 +0700

    test

---
+++ src/routes/UserRoute.js
@@ -0,0 +1,80 @@
+const userHandler = require('../handlers/UserControler');
+const { registerSchema, loginSchema, registerResponseSchema, loginResponseSchema, profileSchema, profileResponseSchema } = require('../validators');
+
+exports.reqLogRoutes = [
+    {
+        method: 'POST',
+        path: '/suca-api/register',
+        options: {
+            auth: false,
+            description: 'Register',
+            notes: 'Register User Account',
+            tags: ['api','reglog'],
+            validate: {
+                payload: registerSchema
+            },
+            plugins: {
+                'hapi-swagger': {
+                    order:1,
+                    responses: {
+                        200: {
+                            description: 'Berhasil',
+                            schema: registerResponseSchema
+                        }
+                    }
+                }
+            },
+            handler: userHandler.register
+        }
+    },
+    {
+        method: 'POST',
+        path: '/suca-api/login',
+        options: {
+            auth: false,
+            description: 'Login',
+            notes: 'Login User Account',
+            tags: ['api','reglog'],
+            validate: {
+                payload: loginSchema
+            },
+            plugins: {
+                'hapi-swagger': {
+                    order:2,
+                    responses: {
+                        200: {
+                            description: 'Berhasil',
+                            schema: loginResponseSchema
+                        }
+                    }
+                }
+            },
+            handler: userHandler.login
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/suca-api/profile/{id}',
+        options: {
+            auth: false,
+            description: 'Edit Profile',
+            notes: 'Update user profile information',
+            tags: ['api', 'profile'],
+            validate: {
+                payload: profileSchema,
+            },
+            plugins: {
+                'hapi-swagger': {
+                    order: 3,
+                    responses: {
+                        200: {
+                            description: 'Profile successfully updated',
+                            schema: profileResponseSchema
+                        }
+                    }
+                }
+            },
+            handler: userHandler.editProfile,
+        },
+    },
+];
