d441e876e6fa18baf9a0e4011d858efdce914528
arifmuhammad12 <97782257+arifmuhammad12@users.noreply.github.com>
2024-12-02 20:28:42 +0700

    Update allRoutes.js

--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -1,8 +1,8 @@
 const { predictionRoutes } = require("./predictionRoute.js");
-const { UserRoute } = require("./UserRoute.js");
+const { reqLogRoutes } = require("./UserRoute.js");
 
 const routes = [
-    ...UserRoute,
+    ...reqLogRoutes,
     ...predictionRoutes
 ];
 
